3fb469d8fb9b99afe6751528c7d55855fa8bf363
KesleyWilie <kesleywilie6@gmail.com>
2025-01-31 23:08:30 -0300

    add dto's

---
+++ src/main/java/com/sistema/dtos/AdministradorDTO.java
@@ -0,0 +1,22 @@
+package com.sistema.dtos;
+
+//AdministradorDTO.java
+
+public class AdministradorDTO {
+	private String username;
+	private String password;
+ 
+ // Getters e Setters
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
---
+++ src/main/java/com/sistema/dtos/ParticipanteDTO.java
@@ -0,0 +1,31 @@
+// ParticipanteDTO.java
+package com.sistema.dtos;
+
+public class ParticipanteDTO {
+    private String nome;
+    private String email;
+    private String password;
+    
+ // Getters e Setters
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+    
+    
+}
