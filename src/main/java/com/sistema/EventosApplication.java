34e72ee54646b24799cbf24448c250f5de82afa3
KesleyWilie <kesleywilie6@gmail.com>
2025-02-04 12:26:14 -0300

    atualização eventos

--- src/main/java/com/sistema/EventosApplication.java
+++ src/main/java/com/sistema/EventosApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SistemaEventosApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SistemaEventosApplication.class, args);
-	}
-
+public class EventosApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EventosApplication.class, args);
+    }
 }
